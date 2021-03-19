b163d8b510ff5145666fdacef6b0866ce5c8d6c1
Jia <pochialu@gmail.com>
2021-03-19 16:50:02 +0800

    feat: create a check to wait for the auth of firebase then initialize

--- src/main.js
+++ src/main.js
@@ -4,4 +4,17 @@ import router from './router'
 
 import './assets/main.css'
 
-createApp(App).use(router).mount('#app')
+// import firebase auth service
+import { projectAuth } from './firebase/config'
+
+let app
+
+projectAuth.onAuthStateChanged(() => {
+  // once first time do it, it has a value
+  if (!app) {
+    app = createApp(App)
+      .use(router)
+      .mount('#app')
+  }
+})
+
