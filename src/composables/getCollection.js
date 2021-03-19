5d48b68009bb9591c79f0da7f89290c208da6564
Jia <pochialu@gmail.com>
2021-03-19 16:58:48 +0800

    feat: create getCollection firebase to get data

---
+++ src/composables/getCollection.js
@@ -0,0 +1,26 @@
+import { ref } from "vue";
+import { projectFirestore } from '../firebase/config'
+
+const getCollection = (collection) => {
+  const documents = ref(null)
+  const error = ref(null)
+  // get the firebase collection and orderby
+  let collectionRef = projectFirestore.collection(collection)
+    .orderBy('createdAt')
+  // once the collection be changes, invoked
+  collectionRef.onSnapshot((snap) => {
+    let results = []
+    snap.docs.forEach(doc => {
+      doc.data().createdAt && results.push({ ...doc.data(), id: doc.id })
+    })
+    documents.value = results
+    error.value = null
+  }, (err) => {
+    console.log(err.message)
+    documents.value = null
+    error.value = "Could not fetch data"
+  })
+  return { documents, error }
+}
+
+export default getCollection
