9f4b0eef78a2a0cfd955e9918eb5636f16e09b3e
Jia <pochialu@gmail.com>
2021-03-19 16:46:44 +0800

    feat: create Navbar component in Chatroom

--- src/views/Chatroom.vue
+++ src/views/Chatroom.vue
@@ -1,7 +1,13 @@
-<template>Chatroom</template>
+<template>
+  <div class="container"><Navbar /></div>
+</template>
 
 <script>
-export default {};
+import Navbar from "../components/Navbar.vue";
+
+export default {
+  components: { Navbar },
+};
 </script>
 
 <style>
